09c67f03bd37dd8f64b29718cf49b7748086f69c
colormono <colormono@gmail.com>
2024-07-26 18:12:48 -0300

    feat: static website

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+
+export const config = {
+  name: 'Daniel Rivas',
+  email: 'daninjafx@gmail.com',
+  url: 'https://daninjafx.com',
+  title: 'Daniel Rivas | Personal Portfolio',
+  description: '3D Art and Motion Graphics',
+  cover: {
+    alt: 'DaninjaFX Portfolio Cover',
+    url: '/images/cover.jpg'
+  },
+  logo: {
+    alt: 'DaninjaFX',
+    url: '/daninjafx-logo.jpg'
+  },
+  social: {
+    instagram: 'https://www.instagram.com/daninjafx/',
+    behance: 'https://behance.net/daninjafx',
+    vimeo: 'https://vimeo.com/daninjafx',
+    youtube: 'https://www.youtube.com/channel/UCYKmKKXAJo55_Q0-Ct_lZsA/videos?view_as=subscriber'
+  },
+  og: {
+    title: 'Daniel Rivas | Personal Portfolio',
+    description: '3D Art and Motion Graphics',
+    url: 'https://daninjafx.com/images/social.jpg'
+  }
+}
